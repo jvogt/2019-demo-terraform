0bd80d3cd8501d8960143be1a53e8c70fbfe6ed7
Jeff Vogt <jvogt@chef.io>
2019-05-05 09:26:10 -0700

    bugfix

--- concourse/common/templates/concourse-final-setup-local.sh
+++ concourse/common/templates/concourse-final-setup-local.sh
@@ -1,4 +1,4 @@
-fly --target demo login --concourse-url ${concourse_url} -u ${concourse_user_name} -p ${concourse_user_password}
+fly --target demo login --concourse-url http://${concourse_url} -u ${concourse_user_name} -p ${concourse_user_password}
 fly --target demo sync
 fly -t demo set-pipeline -p chef-code -c ../chef-pipeline.yml
 fly -t demo set-pipeline -p app-code -c ../app-pipeline.yml
