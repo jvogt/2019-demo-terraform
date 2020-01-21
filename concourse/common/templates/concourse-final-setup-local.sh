1661707fc8b58743c77f763866ea2fa6b7386bf7
Jeff Vogt <jvogt@chef.io>
2020-01-21 12:04:39 -0800

    remove concourse

--- concourse/common/templates/concourse-final-setup-local.sh
+++
@@ -1,5 +0,0 @@
-fly --target demo login --concourse-url http://${concourse_url} -u ${concourse_user_name} -p ${concourse_user_password}
-fly --target demo sync
-fly -t demo set-pipeline -p chef-code -c ../chef-pipeline.yml -n
-fly -t demo set-pipeline -p app-code -c ../app-pipeline.yml -n
-fly -t demo set-pipeline -p packer-code -c ../packer-pipeline.yml -n
