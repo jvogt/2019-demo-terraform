360ae092ed727e85393d2bc55849343ea6350216
Jeff Vogt <jvogt@chef.io>
2020-05-17 21:46:22 -0700

    so i can do other stuff

--- demo_instances/aws/run_command.sh
+++ demo_instances/aws/run_command.sh
@@ -4,3 +4,6 @@ for ip in $(./get_instances.sh $1); do
   ssh -o "StrictHostKeyChecking=no" centos@$ip $2 &
 done
 
+# for ip in $(./get_instances.sh $1); do
+#   knife bootstrap centos@$ip -N $ip --policy_name base --policy_group $1 -y -i ~/Desktop/jvogt-sa-aws.pem --sudo &
+# done
